2f5bbc6b8f9b01d3d5aed30e7b949be17870eae5
technologicaljerry <cserajatverma@gmail.com>
2023-04-10 23:20:44 +0530

    Update localStorage.js

--- localStorage.js
+++ localStorage.js
@@ -10,5 +10,7 @@ function store() {
         alert('Please enter UserName or E-Mail Address');
     } else if (password.value.length == 0) {
         alert('Please enter in Password');
+    } else if (userName.value.length == 0 && password.value.length == 0) {
+        alert('Please enter UserName and Password');
     }
 }
