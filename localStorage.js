ec486aec76f67afc940e7839ab044a5d36e78dc2
technologicaljerry <cserajatverma@gmail.com>
2023-04-10 23:16:49 +0530

    initial commit

---
+++ localStorage.js
@@ -0,0 +1,8 @@
+function store() {
+
+    let userName = document.getElementById('userName');
+    let password = document.getElementById('password');
+    let upperCase = /[A-Z]/g;
+    let lowerCase = /[a-z]/g;
+    let numbers = /[0-9]/g;
+}
